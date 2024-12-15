f7573ec5ffcfda9bfb9950456b0449ed8c2eb796
mitul88 <shahed.rahman88@gmail.com>
2024-12-15 23:43:48 +0600

    token cache expiry config modified

--- src/config/cache.config.js
+++
@@ -1,3 +0,0 @@
-module.exports.CACHE_CONFIG = {
-  EXPIRE_IN_SECOND: 864000, // 1 day
-};
--- src/middleware/rate-limiter.middleware.js
+++ src/middleware/rate-limiter.middleware.js
@@ -1,13 +1,12 @@
 const { CACHE_CONFIG } = require("../config/cache.config");
 const { RATE_LIMITER_CONFIG } = require("../config/rate-limiter.config");
-const { rateLimiterService } = require("../service/rate-limiter.service");
 
 module.exports.rateLimiterMiddleware = {
   tokenBucketLimiter: async (req, res, next) => {
     const clientIp = req.socket.remoteAddress;
     const maxNumOfRequest = RATE_LIMITER_CONFIG.MAX_REQUEST;
     const tokenRefillInterval = RATE_LIMITER_CONFIG.REFILL_INTERVAL;
-    const cacheExpiry = CACHE_CONFIG.EXPIRE_IN_SECOND;
+    const cacheExpiry = RATE_LIMITER_CONFIG.TOKEN_CACHE_EXPIRE_IN_SECOND;
     try {
       let allowed = await rateLimiterService.tokenBucket(
         clientIp,
